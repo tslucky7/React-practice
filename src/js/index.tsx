e9982dbcff9946da72f08fdd22056f44c8cb921a
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-05-03 15:00:57 +0900

    0513 子要素の説明まで

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -4,6 +4,15 @@ import { createRoot } from 'react-dom/client';
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
+// const element = <h1>Hello, react</h1>;
+// const element = <img src="logo.png" alt='logo' />;
+const element = (
+  <div>
+    <h1>Hello, react</h1>
+    <p>Welcome to the React application!</p>
+  </div>
+);
+
 // Render your React component instead
 const root = createRoot(document.getElementById('app')!);
-root.render(<h1>Hello, world</h1>);
+root.render(element);
