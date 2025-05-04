ac2df97afabcde54be1cb07acdf8fc5f27a26241
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-05-04 19:00:38 +0900

    不要な記載削除

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { TodoList } from './components/TodoList';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
 // // Render your React component instead
 const root = createRoot(document.getElementById('app')!);
 root.render(<TodoList />);
