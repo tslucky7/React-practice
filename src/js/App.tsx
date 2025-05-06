b2584c13ec76e2c87983f0a91034d5d9f0d26c7b
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-05-07 00:03:17 +0900

    0541 onChangeで書き換わるところの確認まで

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -3,9 +3,20 @@ import { TodoList } from "./components/TodoList";
 import { Heading } from "./components/Heading";
 
 export const App = () => {
+  const [newTask, setNewTask] = React.useState<string>("掃除");
+
+  console.log(newTask);
+
   return (
     <main className="my-0 mx-auto w-4/5 text-center">
       <Heading level="h1">TODO</Heading>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        className="border-2 border-emerald-500 rounded-md p-2 mt-4"
+      />
+      <p>{newTask}</p>
       <TodoList />
     </main>
   );
