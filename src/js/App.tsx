b48fcac885d785b274f7c224bb481442aa6a5538
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-05-11 17:37:42 +0900

    3006 コードのリファクタリング前

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -8,7 +8,7 @@ export type Todo = {
   task: string;
   deadline: string;
   person: string;
-}
+};
 
 export const App = () => {
   const [todoList, setTodoList] = React.useState<Todo[]>([]);
@@ -17,16 +17,45 @@ export const App = () => {
   const [newPerson, setNewPerson] = React.useState<string>("");
 
   const addNewTodo = () => {
-    setTodoList((prev) => [...prev, {id: Date.now(), task: newTask, deadline: newDeadline, person: newPerson}]);
-  }
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        task: newTask,
+        deadline: newDeadline,
+        person: newPerson,
+      },
+    ]);
+
+    setNewTask("");
+    setNewDeadline("");
+    setNewPerson("");
+  };
 
   return (
     <main className="my-0 mx-auto w-4/5 text-center">
       <Heading level="h1">TODO</Heading>
-      <TextField label="タスク名" value={newTask} onChange={setNewTask} type="text" />
-      <TextField label="締切" value={newDeadline} onChange={setNewDeadline} type="date" />
-      <TextField label="担当者" value={newPerson} onChange={setNewPerson} type="text" />
-      <button className="border bg-cyan-400" onClick={addNewTodo}>追加</button>
+      <TextField
+        label="タスク名"
+        value={newTask}
+        onChange={setNewTask}
+        type="text"
+      />
+      <TextField
+        label="締切"
+        value={newDeadline}
+        onChange={setNewDeadline}
+        type="date"
+      />
+      <TextField
+        label="担当者"
+        value={newPerson}
+        onChange={setNewPerson}
+        type="text"
+      />
+      <button className="border bg-cyan-400" onClick={addNewTodo}>
+        追加
+      </button>
       <TodoList todoList={todoList} />
     </main>
   );
