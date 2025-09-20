8233cba3f0fb08d56a6e1b4d1e583f6778ec8f4f
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-09-20 23:14:45 +0900

    useEffectの仕様確認、依存配列の挙動について学習、マウント・レンダリングの動作について学習

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -7,18 +7,26 @@ import { useEffect } from "react";
 
 export const App = () => {
   const [todoList, setTodoList] = React.useState<Todo[]>([]);
-
+  const [count, setCount] = React.useState<number>(0);
   useEffect(() => {
     console.log("コンポーネントがマウントされた時とtodoListが更新された時に実行される");
 
-    return () => {
-      console.log("コンポーネントがアンマウントされる時に実行される");
-    };
+    // クリーンアップ関数の例
+    // return () => {
+    //   console.log("コンポーネントがアンマウントされる時に実行される");
+    // };
   }, [todoList]);
 
   return (
     <main className="my-0 mx-auto w-3/4 text-center">
       <Heading level="h1">TODO</Heading>
+      <div className="mt-6">
+        <Heading level="h2">カウント</Heading>
+        <div className="mt-8">
+          <p>{count}</p>
+          <button onClick={() => setCount(count + 1)}>カウントアップ</button>
+        </div>
+      </div>
       <div className="mt-6">
         <Heading level="h2">新規TODO作成</Heading>
         <div className="mt-8">
