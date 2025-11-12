2f7735f614e5395dfbe627cd13b707a508409c17
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-11-12 13:21:01 +0900

    feat: useEffectを使ってLocalStorageにデータを保存する
    - tailwindのクラスを追加
    - 削除ボタンを追加
      - 関数定義などは未実装

--- src/js/components/todo/TodoItem.tsx
+++ src/js/components/todo/TodoItem.tsx
@@ -8,10 +8,13 @@ type TodoItemProps = {
 
 export const TodoItem: React.FC<TodoItemProps> = ({ task, deadline, person }) => {
   return (
-    <li className="grid grid-cols-3	">
+    <li className="grid grid-cols-4	">
       <div className="">{task}</div>
-      <div className="">{deadline}</div>
       <div className="">{person}</div>
+      <div className="">{deadline}</div>
+      <button className="border bg-cyan-400 w-16 rounded" onClick={() => {}}>
+        削除
+      </button>
     </li>
   );
 };
