91b40a92fa801077ab177c1ac848e13efc317672
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-11-30 22:12:11 +0900

    12:36 feat: ログイン状態を管理するContextを追加
    - 実際にログイン状態を管理する処理は未実装
    - index.tsxからAuthProviderを適用
    - App.tsx、TodoList.tsx、TodoItem.tsxからAuthContextを利用

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import '../css/main.css'
 import { App } from './App';
+import { AuthProvider } from './contexts/AuthContext';
 
 // // Render your React component instead
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />);
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
