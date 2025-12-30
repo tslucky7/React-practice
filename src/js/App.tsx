70de5b2955bdab68adfef1a9e9421f9071199eca
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-12-31 00:03:57 +0900

    25:38 コンテキストを活用してログイン・ログアウト機能を擬似的に実装
    - ※実際にサーバー側やCookieを活用してのユーザー認証機能ではない。

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -6,17 +6,50 @@ import { useTodoList } from "./components/todo/use-todo-list";
 import { useTimer } from "./components/todo/use-timer";
 import { useContext } from "react";
 import { AuthContext } from "./contexts/AuthContext";
+import { Button } from "./components/parts/Button";
+import { TextField } from "./components/parts/TextField";
 
 export const App = () => {
   const { todoList, addTodo, deleteTodo } = useTodoList();
   const { time } = useTimer();
-  const { isLoggedIn } = useContext(AuthContext);
+  const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useContext(AuthContext);
+
+  if (!isLoggedIn) {
+    return (
+      <main className="my-0 mx-auto w-3/4 text-center">
+        <Heading level="h1">ログイン</Heading>
+        <div className="flex gap-2">
+          <TextField
+            id="user-name"
+            label="ユーザー名"
+            type="text"
+            value={userName}
+            onChange={setUserName}
+          />
+          <Button
+            onClick={() => setIsLoggedIn(true)}
+            color="blue"
+          >
+            ログイン
+          </Button>
+        </div>
+      </main>
+    )
+  }
 
   return (
     <main className="my-0 mx-auto w-3/4 text-center">
       <Heading level="h1">TODO</Heading>
       <div className="">タイマー：{time}秒</div>
-      <div>{isLoggedIn ? "ログイン中" : "ログアウト中"}</div>
+      <div>{isLoggedIn ? `ようこそ、${userName}さん` : "ログアウト中"}</div>
+      <div>
+        <Button
+          onClick={() => setIsLoggedIn(false)}
+          color="red"
+        >
+          ログアウト
+        </Button>
+      </div>
       <div className="mt-6">
         <Heading level="h2">新規TODO作成</Heading>
         <div className="mt-8">
--- src/js/components/parts/Button.tsx
+++ src/js/components/parts/Button.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const Button = ({ onClick, color, children }: React.PropsWithChildren<Props>) => {
   const style = color === "red" ? "bg-red-400" : "bg-cyan-400";
   return (
-    <button className={`border ${style} w-16 rounded`} onClick={onClick}>
+    <button className={`border ${style} w-fit px-1 rounded`} onClick={onClick}>
       {children}
     </button>
   )
